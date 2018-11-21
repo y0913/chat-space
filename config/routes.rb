a8db125b78e2d11611e19aba45194ac688b7acd9
y0913 <yohei@yamashitayouheinoMacBook-puro.local>
2018-11-21 20:59:29 +0900

    Edit chat-space
    チャットスペース送信画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'messages' => 'messages#index'
+  root 'groups#index'
+  get 'messages' => 'messages#index' #後で削除する
+  get 'groups/edit' => 'groups#edit' #後で削除する
+  get '/groups/1/edit' => 'groups#new'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
 end
